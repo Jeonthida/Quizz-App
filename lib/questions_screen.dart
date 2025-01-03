f16dd7433e3a93330008248fcfc3e34316bcfed7
kwankwan1601 <kwankwan1601@gmail.com>
2025-01-03 01:57:45 +0000

    Initial commit from Project IDX

---
+++ lib/questions_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatelessWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.purple, Colors.deepPurple],
+            ),
+          ),
+          child: Column(
+            children: [
+              Text('What are the main building blocks of Flutter UIs?')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
